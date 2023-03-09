e142e0696da8b3c5223612ffe1d5a7ad97ac22bf
taymie-biyi <temiloluwa.olubiyi@yahoo.com>
2023-03-09 11:56:25 -0600

    Complete Exercise Three

---
+++ workspace-busyqa/JavaBasics/src/main/java/WeekOne/ExerciseThree.java
@@ -0,0 +1,18 @@
+package WeekOne;
+
+public class ExerciseThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Write a Java program to divide two numbers and print them on the screen.
+//		For instance: Input: 50 / 20   
+//		       Output: 2.5
+
+		
+		int fig = 50;
+		int get = 20;
+		System.out.println(fig/get);
+	}
+
+}
