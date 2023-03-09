0fc92f5d8c34e691fced155b241824d8493ca2b6
taymie-biyi <temiloluwa.olubiyi@yahoo.com>
2023-03-09 11:51:28 -0600

    Complete Exercise Two

---
+++ workspace-busyqa/JavaBasics/src/main/java/WeekOne/ExerceiseTwo.java
@@ -0,0 +1,18 @@
+package WeekOne;
+
+public class ExerceiseTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Write a Java program to print the sum of two numbers.
+//		For instance:  Input: 74 + 26
+//		         Output: 100
+			
+		
+		int one = 74;
+		int two = 26;
+		
+		System.out.println(one + two);
+	}
+
+}
