c08c10380fa92b83a260a59e5e923d9eda964791
taymie-biyi <temiloluwa.olubiyi@yahoo.com>
2023-03-09 11:47:22 -0600

    Complete Exercise One

---
+++ workspace-busyqa/JavaBasics/src/main/java/WeekOne/ExerciseOne.java
@@ -0,0 +1,14 @@
+package WeekOne;
+
+public class ExerciseOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		a.	Write a Java program to print 'Hello' on screen and then print your name on a separate line. 
+
+		
+			System.out.println("Hello");
+			System.out.println("Temiloluwa");
+	}
+
+}
